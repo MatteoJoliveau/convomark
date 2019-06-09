9a7c406279bb3bd229011dcae793ba2effe9ac1e
Matteo Joliveau <matteo.joliveau@mikamai.com>
2019-06-09 19:11:18 +0200

    Added vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import './plugins/vuetify'
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -6,6 +7,8 @@ import './registerServiceWorker';
 import './assets/application.scss';
 import { createProvider } from './apollo';
 import i18n from './i18n';
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false;
 
