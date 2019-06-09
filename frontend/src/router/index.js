10491f7e884603644ca137e7450371ae04fc0905
Matteo Joliveau <m.joliveau@koor.it>
2019-06-09 12:06:21 +0200

    Updated dependencies

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -24,9 +24,6 @@ export default new Router({
     {
       path: '/login',
       name: 'login',
-      // route level code-splitting
-      // this generates a separate chunk (login.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "login" */ '@/views/Login.vue'),
     },
   ],
