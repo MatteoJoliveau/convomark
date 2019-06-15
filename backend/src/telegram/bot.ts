d0b34d4785abfb8654cf1c759db6a5b76cb58938
Matteo Joliveau <matteo.joliveau@mikamai.com>
2019-06-16 00:44:31 +0200

    Implemented better webhook setup

--- backend/src/telegram/bot.ts
+++ backend/src/telegram/bot.ts
@@ -1,10 +1,15 @@
 import Telegraf, {ContextMessageUpdate} from 'telegraf';
-import {inject, lifeCycleObserver, LifeCycleObserver, CoreBindings, ApplicationConfig} from '@loopback/core';
+import {
+  inject,
+  lifeCycleObserver,
+  LifeCycleObserver,
+  CoreBindings,
+  ApplicationConfig,
+} from '@loopback/core';
 import {Update} from 'telegram-typings';
 import {TelegramBindings} from './keys';
 import {ConvoMarkBindings, ApplicationMode} from '../providers';
 import {Loggable, Logger, logger} from '../logging';
-import { raw } from 'body-parser';
 
 @logger()
 @lifeCycleObserver()
@@ -16,15 +21,13 @@ export class TelegramBot implements LifeCycleObserver, Loggable {
     @inject(TelegramBindings.TELEGRAM_TOKEN) token: string,
     @inject(TelegramBindings.TELEGRAM_SECRET) secret: Buffer,
     @inject(ConvoMarkBindings.APPLICATION_MODE) mode: ApplicationMode,
-    @inject(CoreBindings.APPLICATION_CONFIG) appConfig: ApplicationConfig
+    @inject(CoreBindings.APPLICATION_CONFIG) {domain}: ApplicationConfig,
   ) {
     this.bot = new Telegraf(token);
 
     if (mode === 'production') {
-      const { domain } = appConfig;
       const webhook = `${domain}/bot/updates/${secret.toString('hex')}`;
-      this.logger.info('Registered bot webook', {webhook});
-      this.bot.telegram.setWebhook(webhook);
+      this.setUpWebhook(webhook);
     }
 
     this.bot.on('message', ({from, reply}) => {
@@ -52,5 +55,23 @@ Please try again later`);
     this.logger.info('Stopping the bot');
     this.bot.stop();
     this.logger.info('Stopped the bot');
+  }
+
+  async setUpWebhook(webhook: string): Promise<void> {
+    try {
+      const webhookInfo = await this.bot.telegram.getWebhookInfo();
+      if (webhookInfo.url && webhookInfo.url === webhook) {
+        this.logger.info('Webhook already set', {webhook});
+      } else {
+        const success = await this.bot.telegram.setWebhook(webhook);
+        if (success) {
+          this.logger.info('Registered bot webook', {webhook});
+        } else {
+          this.logger.error('Something went wrong when setting the webhook');
+        }
+      }
+    } catch (e) {
+      this.logger.error(e);
+    }
   }
 }
