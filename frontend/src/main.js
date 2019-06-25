1532f37444551cd72e0ad3889647fa628e218681
Matteo Joliveau <matteo.joliveau@mikamai.com>
2019-06-25 12:09:36 +0200

    Installed Matomo analytics

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Buefy from 'buefy';
+import Matomo from 'vue-matomo';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -11,6 +12,16 @@ import i18n from './i18n';
 Vue.config.productionTip = false;
 
 Vue.use(Buefy);
+
+Vue.user(Matomo, {
+  host: process.env.MATOMO_HOST,
+  siteId: 1,
+  trackerFileName: 'matomo',
+  router,
+  enableLinkTracking: true,
+  requireConsent: true,
+  debug: process.env.CONTEXT !== 'production',
+});
 
 new Vue({
   router,
